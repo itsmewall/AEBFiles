db487991cbe21455b1c45c909e7916a2eda8f9c8
Wallace Oliveira <wallacedeoliveira10@hotmail.com>
2024-03-07 14:23:20 -0300

    Image Viewer

---
+++ static/js/image-modal.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Quando uma imagem é clicada
+    document.querySelectorAll('.photo img').forEach(item => {
+        item.addEventListener('click', event => {
+            const modal = document.getElementById("imageModal");
+            const modalImg = document.getElementById("img01");
+            const captionText = document.getElementById("caption");
+            const imageInfo = document.getElementById("imageInfo");
+            const downloadLink = document.getElementById("downloadLink");
+
+            modal.style.display = "block";
+            modalImg.src = item.src;
+            captionText.innerHTML = item.alt; // Ou qualquer outra informação
+            imageInfo.innerHTML = "Detalhes da Imagem"; // Substitua com informações reais da imagem
+            downloadLink.href = item.src; // Define o link para download
+
+            // Fechar o modal
+            const span = document.getElementsByClassName("close")[0];
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+        });
+    });
+});
